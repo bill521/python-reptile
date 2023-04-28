fabcf51ef1bf129ee29e8c408be7954bf581ecc2
wangdong_02 <1498243830@qq.com>
2023-04-28 15:22:34 +0800

    增加打包程序V10

--- config/SpiderConfig.py
+++ config/SpiderConfig.py
@@ -20,6 +20,7 @@ class SpiderConfig:
         return self
 
     def default_selenium_harder(self):
+
         self.cookie = "ttwid=1%7CvTO_uJ7LmaveRIOE8nlIwh0MF9NA5UKrFYGNozKNQKM%7C1682424504"
         "%7C182d57cf452d8ec06cebbdfab649f8a7251a58db51307e7c24f07f4314e6e675; home_can_add_dy_2_desktop=%220%22; "
         "strategyABtestKey=%221682424506.261%22; passport_csrf_token=4517be9174294276aef4a0270de60b9d; "
